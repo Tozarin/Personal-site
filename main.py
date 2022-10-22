679d1e25cbb122741e9bc86644903357dccff5dc
Startsev Matvey <89902951+Tozarin@users.noreply.github.com>
2022-10-22 19:32:48 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='')
 
 @app.route('/')
 def index():
-    return 'Hi'
-
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    app.run(port=5000, debug=True)
